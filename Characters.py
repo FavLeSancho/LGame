5ab1677c8ae4b07083d6146fc78b58c32a805446
Fav <kekedumas@hotmail.fr>
2016-02-08 22:22:40 +0100

    Update Characters.py

--- Characters.py
+++ Characters.py
@@ -1,1 +1,15 @@
+class WereWolf:
+  def __init__(self):
+  
+  
+class Witch:
+  def __init__(self):
+    
+    
+class Cupidon:
+  def __init__(self):
+    
 
+class Hunter:
+  def __init__(self):
+    
