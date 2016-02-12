6f05e29cf7033c25dd1878d31deed7dffb73eba3
Fav <kekedumas@hotmail.fr>
2016-02-12 09:06:36 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,34 @@
 #Projet LG by Fav/Shirotani/user3
 from tkinter import *
 
-def LoadEntry(Chat, EntryText):
+def FinalMessage(Chat, EntryText):
     if EntryText != '':
         Chat.config(state=NORMAL)
         if Chat.index('end') != None:
-            LineNumber = float(Chat.index('end'))-1.0
+            Ligne = float(Chat.index('end'))-1.0
             Chat.insert(END, "Vous: " + EntryText)
-            Chat.tag_add("Vous", LineNumber, LineNumber+0.4)
+            Chat.tag_add("Vous", Ligne, Ligne+0.4)
             Chat.tag_config("Vous", foreground="#FF8000", font=("Arial", 12, "bold")) #Police, couleur...
             Chat.config(state=DISABLED)
             Chat.yview(END)
 
 
-def FilteredMessage(EntryText):
-    EndFiltered = ''
+def Filtration(EntryText):
+    DoneFilter = ''
     for i in range(len(EntryText)-1,-1,-1):
         if EntryText[i]!='\n':
-            EndFiltered = EntryText[0:i+1]
+            DoneFilter = EntryText[0:i+1]
             break
-    for i in range(0,len(EndFiltered), 1):
-            if EndFiltered[i] != "\n":
-                    return EndFiltered[i:]+'\n'
+    for i in range(0,len(DoneFilter), 1):
+            if DoneFilter[i] != "\n":
+                    return DoneFilter[i:]+'\n'
     return ''
 
-def ClickAction():
-    #Ecrire un message
-    EntryText = FilteredMessage(ChatBox.get("0.0",END))
-    #On envoie le texte dans la fonction LoadEntry, pour ressortir l'effet stylisé, avec le Vous: "message"
-    LoadEntry(Chat, EntryText)
+def ClicAction():
+    #On envoie le texte dans le fonction Filtration
+    EntryText = Filtration(ChatBox.get("0.0",END))
+    #Puis on l'envoie dans la fonction FinalMessage, pour ressortir l'effet stylisé, avec le Vous: "message"
+    FinalMessage(Chat, EntryText)
 
     #Scroll du message
     Chat.yview(END)
@@ -36,11 +36,11 @@ def ClickAction():
     #Supprimer le message de la ChatBox après envoi
     ChatBox.delete("0.0",END)
 
-def PressAction(event):
-	ChatBox.config(state=NORMAL) #Après avoir envoyé un message, on re-initialise la ChatBox
-	ClickAction() #Et on utilise la fonction ClickAction
+def ReleaseEnter(event):
+	ChatBox.config(state=NORMAL) #Après avoir relaché ENTER, on re-initialise la ChatBox (de nouveau dispo)
+	ClicAction() #Et on utilise la fonction ClicAction
 	
-def DisableEntry(event):
+def StopChat(event):
 	#Permet de désactiver le chat quand on appuie sur ENTER (pour eviter de sauter un espace en écrivant)
 	ChatBox.config(state=DISABLED)
 
@@ -60,18 +60,17 @@ scrollbar = Scrollbar(root, command=Chat.yview)
 Chat['yscrollcommand'] = scrollbar.set
 
 #Le Bouton "Envoyer"
-SendButton = Button(root, font=30, text="Envoyer", width="12", height=5, #Customisation du Bouton
+BoutonEnvoi = Button(root, font=30, text="Envoyer", width='12', height='5', #Customisation du Bouton
                     bd=0, bg="#FFBF00", activebackground="#FACC2E", #Customisation du Bouton
-                    command=ClickAction) #Quand on clique, ça lance la fonction "ClicAction"
+                    command=ClicAction) #Quand on clique, ça lance la fonction "ClicAction"
 
 #L'espace où taper son message
-ChatBox = Text(root, bd=0, bg="white",width="29", height="5", font="Arial") # Customisation de la ChatBox
-ChatBox.bind("<Return>", DisableEntry) #Quand on appuie sur Enter, Utiliser la fonction DisableEntry
-ChatBox.bind("<KeyRelease-Return>", PressAction)  #Quand on relache Enter, Utiliser la fonction PressAction
+ChatBox = Text(root, bd=0, bg="white",width='29', height='5', font="Arial") # Customisation de la ChatBox (Taille/police/fond)
+ChatBox.bind("<Return>", StopChat) #Quand on appuie sur Enter, Utiliser la fonction StopChat
+ChatBox.bind("<KeyRelease-Return>", ReleaseEnter)  #Quand on relache Enter, Utiliser la fonction ReleaseEnter
 
 #Placer les différents tools sur l'interface
 scrollbar.place(x=376,y=6, height=386) #La Barre de Scroll
 Chat.place(x=6,y=6, height=386, width=370) #Le cadre du Chat
 ChatBox.place(x=128, y=401, height=50, width=500) #Le cadre de la boite à message (lol)
-SendButton.place(x=6, y=401, height=50) #Le cadre du bouton
-
+BoutonEnvoi.place(x=6, y=401, height=50) #Le cadre du bouton
