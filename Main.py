67b6c93534efe5b70c58bb3fcdca3329a22d2cc4
Fav <kekedumas@hotmail.fr>
2016-02-13 15:26:44 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,23 +15,23 @@ def FinalMessage(Chat, EntryText):
             Chat.yview(END)
 
 
-def Filtration(EntryText):
-    DoneFilter = ''
-    for i in range(len(EntryText)-1,-1,-1):
-        if EntryText[i]!='\n':
-            DoneFilter = EntryText[0:i+1]
-            break
-    for i in range(0,len(DoneFilter), 1):
-            if DoneFilter[i] != "\n":
-                    return DoneFilter[i:]+'\n'
-    return ''
+def Filtration(EntryText): #Permet de filtrer les messages (EntryText)
+    DoneFilter = '' #On initialise la variable qu'on va renvoyer
+    for i in range(len(EntryText)-1,-1,-1): #On va regarder dans notre message (step = -1)
+        if EntryText[i]!='\n': #Si le message n'est pas "vide"
+            DoneFilter = EntryText[0:i+1] #On remplit la varible avec le message (i+1 = limite du message)
+            break #Et on sort de la boucle for
+    for i in range(0,len(DoneFilter), 1): #On va regarder notre message "filtré"
+            if DoneFilter[i] != "\n": #Si il n'est pas "vide"
+                    return DoneFilter[i:]+'\n' #On le 'renvoie', avec un '\n' pour sauter une ligne
+    return '' #Si le message est vide, on ne 'renvoie' rien
 
 def ClicAction():
     #On envoie le texte dans le fonction Filtration
     EntryText = Filtration(ChatBox.get("0.0",END))
-    #Puis on l'envoie dans la fonction FinalMessage, pour ressortir l'effet stylisé, avec le Vous: "message"
-    #FinalMessage(Chat, EntryText)
+    #Puis on l'envoie dans la fonction Command, pour regarder si il y a une éventuelle commande
     Command(EntryText)
+    #FinalMessage(Chat, EntryText)
 
     #Scroll du message
     Chat.yview(END)
@@ -53,21 +53,56 @@ def StopChat(event):
 #---------------GESTION DES COMMANDES---------------#
 #---------------------------------------------------#
 
+def Vote(Player):
+    global JoueurIsVoted,Ordi1IsVoted,Ordi2IsVoted,Ordi3IsVoted,Ordi4IsVoted
+    for Player in PlayerList:
+        if Player == 'Joueur':
+            JoueurIsVoted += 1
+        if Player == 'Ordi1':
+            Ordi1IsVoted += 1
+        if Player == 'Ordi2':
+            Ordi2IsVoted += 1
+        if Player == 'Ordi3':
+            Ordi3IsVoted += 1
+        if Player == 'Ordi4':
+            Ordi4IsVoted += 1
+    
+    
 
 def Command(EntryText):
     if EntryText != '': #Si le texte n'est vide
         if EntryText[0] == '.': #Si le texte commence par un point, on le considère comme une commande (si elle existe)
-            #Début des commandes
+            
+            #Début des commandes#
+            
             if EntryText[:6] == '.vote ':#Commande .vote
-                EntryText = EntryText.replace(".vote ", '')#On enlève le '.vote ' pour ne reccuperer que le pseudo
-                
+                EntryText = EntryText.replace("\n", '') #On enlève le retour à la ligne (fixage de bugs)
+                EntryText = EntryText.replace(".vote ", '') #On enlève le '.vote ' pour ne reccuperer que le pseudo
+                if EntryText in PlayerList: #Si le pseudo fait partie de la liste de Joueurs
+                    Vote(EntryText)
+
+                    #Début du message Chat
+                    VotedMessage = "Vous avez voté contre " + EntryText #Le message à envoyer 
+                    Chat.config(state=NORMAL) # On 'ouvre' le chat
+                    if Chat.index('end') != None:
+                        Ligne = float(Chat.index('end'))-1.0 # On définit la position du message
+                        Chat.insert(END, VotedMessage + '\n') #On l'insert dans le Chat
+                        Chat.tag_add('Start', Ligne, Ligne + 1.100) #On le repère avec Ligne (position)
+                        Chat.tag_config('Start', foreground="#713070", font=("Arial", 15, "bold")) #On lui donne une couleur, taille
+                        Chat.config(state=DISABLED)#Et on 'ferme' le chat
+                        Chat.yview(END)
+                    #Fin du Message Chat
+                else: #Sinon, on ne revoie rien
+                    EntryText = ''
+                    
+
+
             else:
                 FinalMessage(Chat, EntryText) #Si ce n'est pas une commande connue, on envoie le message tel quel
 
         else:
             FinalMessage(Chat, EntryText) # Si le message ne commence pas par un point, on l'envoie normalement
-                
-
+        
 
 #---------------------------------------------------#
 #----------------GESTION DU GRAPHISME---------------#
@@ -111,18 +146,23 @@ BoutonEnvoi.place(x=6, y=401, height=50) #Le cadre du bouton
 #---------------------------------------------------#
 
 PlayerList = ['Player','Ordi1','Ordi2','Ordi3','Ordi4']
-RoleList = ['LoupGarou','Cupidon','Sorciere','Chasseur']
+RoleList = ['LoupGarou','LoupGarou','Cupidon','Sorciere','Chasseur']
 Joueur = random.choice(RoleList)
 RoleList.remove(Joueur)
+JoueurIsVoted = 0
 
 Ordi1 = random.choice(RoleList)
 RoleList.remove(Ordi1)
+Ordi1IsVoted = 0
 
 Ordi2 = random.choice(RoleList)
 RoleList.remove(Ordi2)
+Ordi2IsVoted = 0
 
 Ordi3 = random.choice(RoleList)
 RoleList.remove(Ordi3)
+Ordi3IsVoted = 0
 
 Ordi4 = random.choice(RoleList)
 RoleList.remove(Ordi4)
+Ordi4IsVoted = 0
